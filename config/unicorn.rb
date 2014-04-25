18da8b661f810ac96bd130574fbc34f747320e8a
Darren Oakley <daz.oakley@gmail.com>
2014-04-25 10:49:03 +0100

    Tidy up unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,39 +1,27 @@
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen '/tmp/bandiera.sock', backlog: 64
+port            = Integer(ENV['PORT'] || 5000)
+no_of_processes = Integer(ENV['BANDIERA_PROCESSES'] || 1)
+unix_socket     = ENV['BANDIERA_SOCKET'] || '/tmp/bandiera.sock'
 
-port = ENV['PORT'] || 5000
-listen port.to_i, tcp_nopush: true
+listen            port, tcp_nopush: true
+listen            unix_socket
+timeout           15
+preload_app       true
+worker_processes  no_of_processes
 
-# nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
-
-# combine Ruby 2.0.0 or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-# Switch the unicorn logger.
 if ENV['BANDIERA_SYSLOG']
   require 'syslog-logger'
 
-  class ::Logger::Syslog; alias_method :write, :info; end
+  class Logger::Syslog; alias_method :write, :info; end
 
   syslog_logger       = Logger::Syslog.new('bandiera', Syslog::LOG_LOCAL0)
   syslog_logger.level = Logger::INFO
   logger syslog_logger
 end
 
-no_of_processes = ENV['BANDIERA_PROCESSES'] || 1
-worker_processes no_of_processes.to_i
-
 if ENV['BANDIERA_DIR']
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
   working_directory ENV['BANDIERA_DIR']
 end
 
 if ENV['BANDIERA_PID']
-  # feel free to point this anywhere accessible on the filesystem
   pid ENV['BANDIERA_PID']
 end
