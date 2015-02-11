b07febe63db8e88a8b726d8b46cd6f294726d73d
Darren Oakley <daz.oakley@gmail.com>
2015-02-11 13:50:14 +0000

    Move sinatra controller code into methods
    This allows CodeClimate to analyse the code and suggest improvements.

--- lib/bandiera/api_v1.rb
+++ lib/bandiera/api_v1.rb
@@ -1,6 +1,10 @@
 module Bandiera
   class APIv1 < WebAppBase
     get '/groups' do
+      _get_groups
+    end
+
+    def _get_groups
       add_statsd_timer 'api.v1.groups.get'
 
       groups = feature_service.get_groups.map { |group| { name: group.name } }
@@ -8,6 +12,10 @@ module Bandiera
     end
 
     post '/groups' do
+      _post_groups
+    end
+
+    def _post_groups
       add_statsd_timer 'api.v1.groups.post'
 
       group_params = params.fetch('group', {})
@@ -23,6 +31,10 @@ module Bandiera
     end
 
     get '/groups/:group_name/features' do |group_name|
+      _get_group_features(group_name)
+    end
+
+    def _get_group_features(group_name)
       add_statsd_timer 'api.v1.group_features.get'
 
       features = feature_service.get_group_features(group_name)
@@ -30,6 +42,10 @@ module Bandiera
     end
 
     post '/groups/:group_name/features' do |group_name|
+      _post_group_features(group_name)
+    end
+
+    def _post_group_features(group_name)
       add_statsd_timer 'api.v1.group_features.post'
 
       feature_params = process_v1_feature_params(params.fetch('feature', {}).merge('group' => group_name))
@@ -42,9 +58,13 @@ module Bandiera
     end
 
     get '/groups/:group_name/features/:feature_name' do |group_name, feature_name|
+      _get_individial_feature(group_name, feature_name)
+    end
+
+    def _get_individial_feature(group_name, feature_name)
       add_statsd_timer 'api.v1.individual_feature.get'
 
-      data, feature, warning = {}, nil, nil
+      data, feature  = {}, nil
 
       begin
         feature = feature_service.get_feature(group_name, feature_name)
@@ -59,6 +79,10 @@ module Bandiera
     end
 
     put '/groups/:group_name/features/:feature_name' do |group_name, feature_name|
+      _put_individial_feature(group_name, feature_name)
+    end
+
+    def _put_individial_feature(group_name, feature_name)
       add_statsd_timer 'api.v1.individual_feature.put'
 
       feature_params         = process_v1_feature_params(params.fetch('feature', {}))
@@ -72,6 +96,10 @@ module Bandiera
     end
 
     get '/all' do
+      _get_all
+    end
+
+    def _get_all
       add_statsd_timer 'api.v1.all.get'
 
       group_data = feature_service.get_groups.map do |group|
@@ -84,7 +112,7 @@ module Bandiera
       render_json(groups: group_data)
     end
 
-    error *[Bandiera::FeatureService::GroupNotFound, Bandiera::FeatureService::FeatureNotFound] do
+    error [Bandiera::FeatureService::GroupNotFound, Bandiera::FeatureService::FeatureNotFound] do
       status 404
       render_json(error: request.env['sinatra.error'].message)
     end
