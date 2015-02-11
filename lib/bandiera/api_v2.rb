c9ee9dab93bb23475b3bd21fa31675533c101df8
Darren Oakley <daz.oakley@gmail.com>
2015-02-11 12:25:15 +0000

    Update APIv2 to work with new Feature control flow
    Removed the gratuitous error rescuing and made use of the new 
    ‘report_enabled_warnings’ method.

--- lib/bandiera/api_v2.rb
+++ lib/bandiera/api_v2.rb
@@ -4,7 +4,7 @@ module Bandiera
       add_statsd_timer 'api.v2.all.get'
 
       group_map = {}
-      warnings  = { user_group: [], user_percentage: [] }
+      warnings  = { user_group: [], user_id: [] }
 
       feature_service.get_groups.each do |group|
         features              = feature_service.get_group_features(group.name)
@@ -38,17 +38,16 @@ module Bandiera
     get '/groups/:group_name/features/:feature_name' do |group_name, feature_name|
       add_statsd_timer 'api.v2.individual_feature.get'
 
+      response = { response: false }
+
       begin
         feature  = feature_service.get_feature(group_name, feature_name)
         response = { response: response_for(feature) }
-      rescue *
-        [
-          Bandiera::FeatureService::GroupNotFound,
-          Bandiera::FeatureService::FeatureNotFound,
-          Bandiera::Feature::UserGroupArgumentError,
-          Bandiera::Feature::UserPercentageArgumentError
-        ] => e
-        response = { response: false, warning:  e.message }
+
+        warnings           = warnings_for(feature)
+        response[:warning] = build_single_warning_reponse(warnings) if warnings.any?
+      rescue Bandiera::FeatureService::FeatureNotFound, Bandiera::FeatureService::GroupNotFound => e
+        response[:warning] = e.message
       end
 
       json_or_jsonp(response)
@@ -60,28 +59,36 @@ module Bandiera
       feature.enabled?(user_group: params[:user_group], user_id: params[:user_id])
     end
 
-    def build_features_hash(features, error_map = { user_group: [], user_percentage: [] })
+    def warnings_for(feature)
+      feature.report_enabled_warnings(user_group: params[:user_group], user_id: params[:user_id])
+    end
+
+    def build_single_warning_reponse(warnings)
+      if warnings.include?(:user_group) && warnings.include?(:user_id)
+        'This feature is configured for both user groups and percentages - you must supply both `user_group` and `user_id` params'
+      elsif warnings.include?(:user_group)
+        'This feature is configured for user groups - you must supply a `user_group` param'
+      elsif warnings.include?(:user_id)
+        'This feature is configured for user percentages - you must supply a `user_id` param'
+      end
+    end
+
+    def build_features_hash(features, warning_map = { user_group: [], user_id: [] })
       response_map = {}
 
       features.each do |feature|
-        begin
-          response_map[feature.name] = response_for(feature)
-        rescue *[Bandiera::Feature::UserGroupArgumentError, Bandiera::Feature::UserPercentageArgumentError] => e
-          map_key = case e
-                    when Bandiera::Feature::UserGroupArgumentError      then :user_group
-                    when Bandiera::Feature::UserPercentageArgumentError then :user_percentage
-                    end
+        response_map[feature.name] = response_for(feature)
 
-          response_map[feature.name] = false
-          error_map[map_key] << { name: feature.name, group: feature.group.name }
+        warnings_for(feature).each do |warning|
+          warning_map[warning] << { name: feature.name, group: feature.group.name }
         end
       end
 
-      [response_map, error_map]
+      [response_map, warning_map]
     end
 
     def warnings_found?(error_map)
-      error_map[:user_group].any? || error_map[:user_percentage].any?
+      error_map[:user_group].any? || error_map[:user_id].any?
     end
 
     def names_for_warnings(warnings, show_group)
@@ -96,8 +103,8 @@ module Bandiera
     def format_multiple_warning_messages(error_map, show_group = false)
       msg           = "The following warnings were raised on this request:\n"
       error_handles = {
-        user_group:      'these features have user groups configured and require a `user_group` param',
-        user_percentage: 'these features have user percentages configured and require a `user_id` param'
+        user_group: 'these features have user groups configured and require a `user_group` param',
+        user_id:    'these features have user percentages configured and require a `user_id` param'
       }
 
       error_handles.each do |type, preamble|
