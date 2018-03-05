89e7bb479bacf48d6c9cebeec90bfe874bcd6470
James Shiell <james@infernus.org>
2018-03-05 13:18:55 +0000

    Changed indentation to satisfy the hound

--- lib/bandiera/web_app_base.rb
+++ lib/bandiera/web_app_base.rb
@@ -12,10 +12,10 @@ module Bandiera
       enable :raise_errors if ENV['AIRBRAKE_API_KEY'] && ENV['AIRBRAKE_PROJECT_ID']
 
       audit_log = if ENV['RECORD_AUDIT_RECORDS'] && ENV['RECORD_AUDIT_RECORDS'].downcase == 'true'
-        LoggingAuditLog.new(recording = record_audit_records)
-      else
-        BlackholeAuditLog.new
-      end
+                    LoggingAuditLog.new(recording = record_audit_records)
+                  else
+                    BlackholeAuditLog.new
+                  end
 
       set :feature_service, CachingFeatureService.new(FeatureService.new(audit_log),
         cache_size: ENV['CACHE_SIZE']&.to_i,
